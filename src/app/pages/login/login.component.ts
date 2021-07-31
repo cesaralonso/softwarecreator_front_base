38a9ac520511d6285336dd8077b446732a0ddadc
cesarmagana <cesar.desarrollo.web@gmail.com>
2021-07-30 21:51:03 -0500

    package updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,8 @@
-import { LoginResponseInterface } from './login-response.interface';
+import { PushNotificationService } from './../../shared/services/push-notifications.service';
 import { LoginInterface } from './login.interface';
 import { AuthService } from './../../shared/services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
-import { LocalStorageService } from 'angular-2-local-storage';
 import { EmailValidator } from '../../theme/validators';
 
 @Component({
@@ -13,42 +10,48 @@ import { EmailValidator } from '../../theme/validators';
   templateUrl: './login.html',
   styleUrls: ['./login.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   items: any;
   form: FormGroup;
   email: AbstractControl;
   password: AbstractControl;
-  recordarSesion: AbstractControl;
   submitted: boolean = false;
 
   constructor(fb: FormBuilder,
               protected service: AuthService, 
-              private localStorageService: LocalStorageService,
-              private toastrService: ToastrService,
-              private router: Router) {
-
-    const recordar = (localStorage.getItem('recordarSesion') === 'true') ? true : false;
+              private pushNotificationService: PushNotificationService,
+              private authService: AuthService) {
 
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, EmailValidator.validate])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-      'recordarSesion': [recordar]
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
     });
 
     this.email = this.form.controls['email'];
     this.password = this.form.controls['password'];
-    this.recordarSesion = this.form.controls['recordarSesion'];
+    
+    if (this.authService.loggedIn()) {
+      this.authService.navigateToFirstModule();
+    }
+  }
+
+  ngOnInit() {
   }
 
   onSubmit(values: LoginInterface): void {
     this.submitted = true;
     if (this.form.valid) {
+      values.recordarSesion = true;
       this.service
         .login(values)
         .subscribe(
-            (response: LoginResponseInterface) => console.log(response));
+            (response: any) => { 
+              console.log(response); 
+              if (response.success) {
+                this.pushNotificationService.addPushSubscriber(response.iduser, response.idrol);
+              }
+           });
     }
   }
-
 
 }
